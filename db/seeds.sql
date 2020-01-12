7af20046e707ba7ef109298375a1c3a7180d5a6d
dimple shanbhag <dimpleshanbhag@dimples-MacBook-Pro.local>
2020-01-11 22:12:58 -0800

    buger app commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) values ("Luger Burger",TRUE);
+INSERT INTO burgers (burger_name, devoured) values ("LePigeon Burger",FALSE);
+INSERT INTO burgers (burger_name, devoured) values ("Double Animal Style Burger",TRUE);
